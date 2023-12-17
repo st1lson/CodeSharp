ab7f1cb004e2fc62830556d0a8a08522b490f4f3
Maksym <rendon4ik@gmail.com>
2023-12-17 17:18:44 +0200

    Add CORS

--- samples/CodeSharp.Samples.WebAPI/Program.cs
+++ samples/CodeSharp.Samples.WebAPI/Program.cs
@@ -5,6 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddCodeSharp();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        builder => builder.WithOrigins("http://localhost:3000")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,5 +32,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowFrontend");
 
 app.Run();
