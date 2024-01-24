0de55a193d6dbbadcd1fff58adf0f8e0b4a6f83a
Maksym <rendon4ik@gmail.com>
2024-01-24 18:49:25 +0200

    Add basic stuff for integration with ASP.NET Core

---
+++ src/CodeSharp.AspNetCore/CodeSharpBuilder.cs
@@ -0,0 +1,106 @@
+﻿using CodeSharp.Core.Executors;
+using CodeSharp.Core.Models;
+using CodeSharp.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CodeSharp.AspNetCore;
+
+public class CodeSharpBuilder<TCompilationLog, TTest, TTestLog>
+{
+    private readonly IServiceCollection _services;
+
+    public CodeSharpBuilder(IServiceCollection services)
+    {
+        _services = services;
+    }
+
+    internal void AddDefaultImplementations()
+    {
+        AddCompilationService<CompilationService<CompilationLog>>();
+        AddCompileExecutor<CompileExecutor<CompilationLog>>();
+    }
+
+    public CodeSharpBuilder<TCompilationLog, TTest, TTestLog> AddCompilationService<TCompilationService>() where TCompilationService : class
+    {
+        var compilationLogType = typeof(TCompilationLog);
+
+        var keyType = GetKeyType(compilationLogType);
+
+        var serviceType = typeof(ICompilationService<,>).MakeGenericType(compilationLogType, keyType);
+        var implementationType = typeof(TCompilationService);
+
+        RegisterImplementations(serviceType, implementationType);
+
+        return this;
+    }
+
+    //public CodeSharpBuilder<TCompilationLog, TTest, TTestLog> AddCompilationLogStore<TStore>() where TStore : class
+    //{
+    //    var compilationLogType = typeof(TCompilationLog);
+
+    //    var keyType = GetKeyType(compilationLogType);
+
+    //    var storeType = typeof(ICompilationLogStore<,>).MakeGenericType(compilationLogType, keyType);
+    //    var implementationType = typeof(TStore);
+
+    //    RegisterImplementations(storeType, implementationType);
+
+    //    return this;
+    //}
+
+    public CodeSharpBuilder<TCompilationLog, TTest, TTestLog> AddCompileExecutor<TCompileExecutor>() where TCompileExecutor : class
+    {
+        var codeExecutorType = typeof(ICompileExecutor<>).MakeGenericType(typeof(TCompilationLog));
+        var implementationType = typeof(TCompileExecutor);
+
+        RegisterImplementations(codeExecutorType, implementationType);
+
+        return this;
+    }
+
+    public CodeSharpBuilder<TCompilationLog, TTest, TTestLog> AddTestExecutor<TTestExecutor>() where TTestExecutor : class
+    {
+        var testExecutorType = typeof(ITestExecutor<>).MakeGenericType(typeof(TCompilationLog));
+        var implementationType = typeof(TTestExecutor);
+
+        RegisterImplementations(testExecutorType, implementationType);
+
+        return this;
+    }
+
+    private void RegisterImplementations(Type serviceType, Type implementationType)
+    {
+        if (!serviceType.IsAssignableFrom(implementationType))
+        {
+            throw new ArgumentException($"{implementationType} must implement {serviceType}.");
+        }
+
+        var assembly = Assembly.GetExecutingAssembly();
+
+        var implementations = assembly.GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface &&
+                        serviceType.IsAssignableFrom(t) && t != implementationType)
+            .ToList();
+
+        if (implementations.Any())
+        {
+            foreach (var implType in implementations)
+            {
+                _services.AddScoped(serviceType, implType);
+            }
+        }
+        else
+        {
+            _services.AddScoped(serviceType, implementationType);
+        }
+    }
+
+    private static Type GetKeyType(Type entity)
+    {
+        var idProperty = entity.GetProperties().FirstOrDefault(p => p.Name == "Id")
+            ?? throw new InvalidOperationException($"The type {entity} must have a property named 'Id'.");
+
+        return idProperty.PropertyType;
+    }
+}
---
+++ src/CodeSharp.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using CodeSharp.Core.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeSharp.AspNetCore;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCodeSharp<TCompilationLog, TTest, TTestLog>(this IServiceCollection services, Action<CodeSharpBuilder>? configureBuilder = null)
+        where TCompilationLog : class
+        where TTest : class
+        where TTestLog : class
+    {
+        var builder = new CodeSharpBuilder(typeof(TCompilationLog), typeof(TTest), typeof(TTestLog), services);
+
+        builder.AddDefaultImplementations();
+
+        configureBuilder?.Invoke(builder);
+
+        return services;
+    }
+
+    public static IServiceCollection AddCodeSharp(this IServiceCollection services, Action<CodeSharpBuilder>? configureBuilder = null)
+    {
+        return services.AddCodeSharp<CompilationLog, Test, TestLog>(configureBuilder);
+    }
+}
