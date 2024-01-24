b93a348f6104b25aa6c365ec1f167b4ccb2a978a
Maksym <rendon4ik@gmail.com>
2024-01-24 17:38:11 +0200

    Add basic setup for ASP.NET Core

---
+++ src/CodeSharp.EntityFramework.AspNetCore/CodeSharpBuilder.cs
@@ -0,0 +1,70 @@
+﻿using CodeSharp.Core.Contracts;
+using CodeSharp.Core.Executors;
+using CodeSharp.Core.Services;
+using CodeSharp.EntityFramework.Stores;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CodeSharp.EntityFramework.AspNetCore;
+
+public class CodeSharpBuilder
+{
+    private readonly IServiceCollection _services;
+
+    public CodeSharpBuilder(IServiceCollection services)
+    {
+        _services = services;
+    }
+
+    internal void AddDefaultImplementations()
+    {
+        AddCompilationService(typeof(ICompilationService<,>), typeof(CompilationService<,>));
+        AddCompilationLogStore(typeof(ICompilationLogStore<,>), typeof(CompilationLogStore<,,>));
+        AddCompileExecutor(typeof(ICompileExecutor<>), typeof(CompileExecutor<>));
+    }
+
+    public CodeSharpBuilder AddCompilationService(Type serviceType, Type implementationType)
+    {
+        RegisterImplementations(serviceType, implementationType);
+        return this;
+    }
+
+    public CodeSharpBuilder AddCompilationLogStore(Type serviceType, Type implementationType)
+    {
+        RegisterImplementations(serviceType, implementationType);
+        return this;
+    }
+
+    public CodeSharpBuilder AddCompileExecutor(Type serviceType, Type implementationType)
+    {
+        RegisterImplementations(serviceType, implementationType);
+        return this;
+    }
+
+    public IServiceCollection Build()
+    {
+        return _services;
+    }
+
+    private void RegisterImplementations(Type serviceType, Type implementationType)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        var implementations = assembly.GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface &&
+                        serviceType.IsAssignableFrom(t) && t != implementationType)
+            .ToList();
+
+        if (implementations.Any())
+        {
+            foreach (var implType in implementations)
+            {
+                _services.AddScoped(serviceType, implType);
+            }
+        }
+        else
+        {
+            _services.AddScoped(serviceType, implementationType);
+        }
+    }
+}
---
+++ src/CodeSharp.EntityFramework.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeSharp.EntityFramework.AspNetCore;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCodeSharp(this IServiceCollection services, Action<CodeSharpBuilder>? configureBuilder = null)
+    {
+        var builder = new CodeSharpBuilder(services);
+
+        builder.AddDefaultImplementations();
+
+        configureBuilder?.Invoke(builder);
+
+        return services;
+    }
+}
