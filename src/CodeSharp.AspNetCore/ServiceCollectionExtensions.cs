6f022b5c5595ba8ce46fabba8b0a6def27607963
Maksym <rendon4ik@gmail.com>
2024-01-26 18:37:29 +0200

    Add DI registration for container configuration

--- src/CodeSharp.AspNetCore/ServiceCollectionExtensions.cs
+++ src/CodeSharp.AspNetCore/ServiceCollectionExtensions.cs
@@ -1,26 +1,33 @@
-﻿using CodeSharp.Core.Models;
+﻿using CodeSharp.Core.Docker.Models;
+using CodeSharp.Core.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CodeSharp.AspNetCore;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddCodeSharp<TCompilationLog, TTest, TTestLog>(this IServiceCollection services, Action<CodeSharpBuilder>? configureBuilder = null)
+    public static IServiceCollection AddCodeSharp<TCompilationLog, TTest, TTestLog>(this IServiceCollection services, Action<ContainerConfiguration>? configuration = null)
         where TCompilationLog : class
         where TTest : class
         where TTestLog : class
     {
-        var builder = new CodeSharpBuilder(typeof(TCompilationLog), typeof(TTest), typeof(TTestLog), services);
+        var builder = new CodeSharpBuilder<TCompilationLog, TTest, TTestLog>(services);
+
+        services.AddSingleton(provider =>
+        {
+            var defaultConfig = new ContainerConfiguration();
+            configuration?.Invoke(defaultConfig);
+
+            return defaultConfig;
+        });
 
         builder.AddDefaultImplementations();
-
-        configureBuilder?.Invoke(builder);
 
         return services;
     }
 
-    public static IServiceCollection AddCodeSharp(this IServiceCollection services, Action<CodeSharpBuilder>? configureBuilder = null)
+    public static IServiceCollection AddCodeSharp(this IServiceCollection services, Action<ContainerConfiguration>? configuration = null)
     {
-        return services.AddCodeSharp<CompilationLog, Test, TestLog>(configureBuilder);
+        return services.AddCodeSharp<CompilationLog, Test, TestLog>(configuration);
     }
 }
