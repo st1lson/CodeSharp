7af1b519b9e81c3b9a3a0ad5dd508ba6184665c9
Maksym <rendon4ik@gmail.com>
2024-03-11 16:21:55 +0200

    Add tests for DockerContainer

--- tests/CodeSharp.Core.Tests/Docker/DockerContainerTests.cs
+++ tests/CodeSharp.Core.Tests/Docker/DockerContainerTests.cs
@@ -1,10 +1,13 @@
 ﻿using AutoFixture;
 using CodeSharp.Core.Docker;
+using CodeSharp.Core.Docker.Exceptions;
+using CodeSharp.Core.Docker.Factories;
 using CodeSharp.Core.Docker.Models;
 using CodeSharp.Core.Docker.Providers;
 using Docker.DotNet;
 using Docker.DotNet.Models;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using Xunit;
 
 namespace CodeSharp.Core.Tests.Docker;
@@ -16,40 +19,191 @@ public class DockerContainerTests
     private readonly IContainerPortProvider _portProvider = Substitute.For<IContainerPortProvider>();
     private readonly IContainerHealthCheckProvider _healthCheckProvider = Substitute.For<IContainerHealthCheckProvider>();
     private readonly IDockerClient _dockerClient = Substitute.For<IDockerClient>();
+    private readonly IDockerClientFactory _dockerClientFactory = Substitute.For<IDockerClientFactory>();
     private readonly ContainerConfiguration _configuration = new() { Image = Image.Default };
 
     private readonly IFixture _fixture = new Fixture();
 
     public DockerContainerTests()
     {
-        _dockerContainer = new DockerContainer(_configuration, _nameProvider, _portProvider, _healthCheckProvider);
+        _dockerClientFactory.CreateClient().Returns(_dockerClient);
+        _nameProvider.GetName().Returns(_fixture.Create<string>());
+
+        _dockerContainer = new DockerContainer(
+            _configuration,
+            _nameProvider,
+            _portProvider,
+            _healthCheckProvider,
+            _dockerClientFactory);
     }
 
     [Fact]
-    public async Task StartAsync_PullsImage_IfNotExists()
+    public async Task StartAsync_WhenImageExists_DoesNotPullImage()
     {
         // Arrange
-        _nameProvider.GetName().Returns("container-name");
-        _portProvider.CurrentPort.Returns(1234);
-        _dockerClient.Images.ListImagesAsync(Arg.Any<ImagesListParameters>(), default)
-            .Returns(Task.FromResult(new List<ImagesListResponse>() as IList<ImagesListResponse>));
+        var cancellationToken = new CancellationToken();
 
-        var createContainerResponse = _fixture.Create<CreateContainerResponse?>();
+        _dockerClient.Images.ListImagesAsync(Arg.Any<ImagesListParameters>(), cancellationToken)
+            .Returns(Task.FromResult<IList<ImagesListResponse>>(new List<ImagesListResponse>
+            {
+                new() { RepoTags = new List<string> { _configuration.Image.ToString() } }
+            }));
 
-        _dockerClient.Containers.CreateContainerAsync(_fixture.Create<CreateContainerParameters>(), CancellationToken.None)
-            .Returns(createContainerResponse);
+        var createContainerResponse = _fixture.Create<CreateContainerResponse>();
 
-        _dockerContainer.CreateContainerAsync(CancellationToken.None)
+        _dockerClient.Containers.CreateContainerAsync(Arg.Any<CreateContainerParameters>(), cancellationToken)
             .Returns(Task.FromResult(createContainerResponse));
 
         // Act
-        await _dockerContainer.StartAsync();
+        await _dockerContainer.StartAsync(cancellationToken);
 
         // Assert
-        await _dockerClient.Images.Received(1)
-            .CreateImageAsync(Arg.Is<ImagesCreateParameters>(args => args.FromImage == _configuration.Image.ToString()),
-                              Arg.Any<AuthConfig>(), Arg.Any<Progress<JSONMessage>>(), default);
+        await _dockerClient.Images.DidNotReceive().CreateImageAsync(
+            Arg.Any<ImagesCreateParameters>(),
+            Arg.Any<AuthConfig>(),
+            Arg.Any<Progress<JSONMessage>>(),
+            cancellationToken);
+        await _dockerClient.Containers.Received(1).StartContainerAsync(
+            Arg.Is<string>(id => id == createContainerResponse.ID),
+            Arg.Any<ContainerStartParameters>(),
+            cancellationToken);
     }
 
-}
+    [Fact]
+    public async Task StartAsync_WhenImageDoesNotExist_PullsImage()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
 
+        _dockerClient.Images.ListImagesAsync(Arg.Any<ImagesListParameters>(), cancellationToken)
+            .Returns(Task.FromResult<IList<ImagesListResponse>>(new List<ImagesListResponse>()));
+
+        var createContainerResponse = _fixture.Create<CreateContainerResponse>();
+
+        _dockerClient.Containers.CreateContainerAsync(Arg.Any<CreateContainerParameters>(), cancellationToken)
+            .Returns(Task.FromResult(createContainerResponse));
+
+        // Act
+        await _dockerContainer.StartAsync(cancellationToken);
+
+        // Assert
+        await _dockerClient.Images.Received(1).CreateImageAsync(
+            Arg.Any<ImagesCreateParameters>(),
+            Arg.Any<AuthConfig>(),
+            Arg.Any<Progress<JSONMessage>>(),
+            cancellationToken);
+        await _dockerClient.Containers.Received(1).StartContainerAsync(
+            Arg.Is<string>(id => id == createContainerResponse.ID),
+            Arg.Any<ContainerStartParameters>(),
+            cancellationToken);
+    }
+
+    [Fact]
+    public async Task StartAsync_WhenImagePullFails_ThrowsException()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+
+        _dockerClient.Images.ListImagesAsync(Arg.Any<ImagesListParameters>(), cancellationToken)
+            .Returns(Task.FromResult<IList<ImagesListResponse>>(new List<ImagesListResponse>()));
+        _dockerClient.Images.CreateImageAsync(Arg.Any<ImagesCreateParameters>(), Arg.Any<AuthConfig>(), Arg.Any<Progress<JSONMessage>>(), cancellationToken)
+            .Throws(new DockerContainerException("Failed to pull image"));
+
+        // Act & Assert
+        await Assert.ThrowsAsync<DockerContainerException>(() => _dockerContainer.StartAsync(cancellationToken));
+    }
+
+
+    [Fact]
+    public async Task EnsureCreatedAsync_WhenCalled_InvokesHealthCheckProvider()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+
+        var createContainerResponse = _fixture.Create<CreateContainerResponse>();
+
+        _dockerClient.Containers.CreateContainerAsync(Arg.Any<CreateContainerParameters>(), cancellationToken)
+            .Returns(Task.FromResult(createContainerResponse));
+
+        await _dockerContainer.StartAsync(cancellationToken);
+
+        // Act
+        await _dockerContainer.EnsureCreatedAsync(cancellationToken);
+
+        // Assert
+        await _healthCheckProvider.Received(2).EnsureCreatedAsync(cancellationToken);
+    }
+
+    [Fact]
+    public async Task EnsureCreatedAsync_ThrowsExceptionIfDockerClientIsNull()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+
+        // Act & Assert
+        await Assert.ThrowsAsync<DockerContainerException>(() => new DockerContainer(
+            _configuration,
+            _nameProvider,
+            _portProvider,
+            _healthCheckProvider,
+            _dockerClientFactory).EnsureCreatedAsync(cancellationToken));
+    }
+
+    [Fact]
+    public async Task StopAsync_WhenCalled_StopsAndRemovesContainer()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+
+        var createContainerResponse = _fixture.Create<CreateContainerResponse>();
+
+        _dockerClient.Containers.CreateContainerAsync(Arg.Any<CreateContainerParameters>(), cancellationToken)
+            .Returns(Task.FromResult(createContainerResponse));
+
+        await _dockerContainer.StartAsync(cancellationToken);
+
+        // Act
+        await _dockerContainer.StopAsync(cancellationToken);
+
+        // Assert
+        await _dockerClient.Containers.Received(1).StopContainerAsync(createContainerResponse.ID, Arg.Any<ContainerStopParameters>(), cancellationToken);
+    }
+
+    [Fact]
+    public async Task StopAsync_ThrowsExceptionIfDockerClientIsNull()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+
+        // Act & Assert
+        await Assert.ThrowsAsync<DockerContainerException>(() => new DockerContainer(
+            _configuration,
+            _nameProvider,
+            _portProvider,
+            _healthCheckProvider,
+            _dockerClientFactory).StopAsync(cancellationToken));
+    }
+
+    [Fact]
+    public async Task Dispose_CleansUpAndDisposesResources()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+
+        var createContainerResponse = _fixture.Create<CreateContainerResponse>();
+        _dockerClient.Containers.CreateContainerAsync(Arg.Any<CreateContainerParameters>(), cancellationToken)
+            .Returns(Task.FromResult(createContainerResponse));
+        _dockerClient.Images.ListImagesAsync(Arg.Any<ImagesListParameters>(), cancellationToken)
+            .Returns(Task.FromResult<IList<ImagesListResponse>>(new List<ImagesListResponse>()));
+        await _dockerContainer.StartAsync(cancellationToken);
+
+        // Act
+        _dockerContainer.Dispose();
+
+        // Assert
+        await _dockerClient.Containers.Received(1).StopContainerAsync(createContainerResponse.ID, Arg.Any<ContainerStopParameters>(), cancellationToken);
+        await _dockerClient.Containers.Received(1).RemoveContainerAsync(createContainerResponse.ID, Arg.Any<ContainerRemoveParameters>(), cancellationToken);
+        _portProvider.Received(1).ReleasePort();
+        _dockerClient.Received(1).Dispose();
+    }
+}
