c4372b4c055fb82782a9f6cdf0f56cc5796fb735
Maksym <rendon4ik@gmail.com>
2024-03-27 11:48:55 +0200

    Add configurations for EF Core entities

---
+++ src/CodeSharp.EntityFramework/Configurations/CompilationLogConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using CodeSharp.Core.Executors.Models.Shared;
+using CodeSharp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Text.Json;
+
+namespace CodeSharp.EntityFramework.Configurations;
+
+public class CompilationLogConfiguration : IEntityTypeConfiguration<CompilationLog>
+{
+    public void Configure(EntityTypeBuilder<CompilationLog> builder)
+    {
+        var jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var converter = new ValueConverter<CodeAnalysisReport, string>(
+                   v => JsonSerializer.Serialize(v, jsonSerializerOptions),
+                   v => JsonSerializer.Deserialize<CodeAnalysisReport>(v, jsonSerializerOptions) ?? new CodeAnalysisReport());
+
+        builder
+            .Property(e => e.CodeReport)
+            .HasConversion(converter)
+            .IsUnicode(false);
+
+        builder
+            .HasKey(e => e.Id);
+    }
+}
---
+++ src/CodeSharp.EntityFramework/Configurations/TestLogConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using CodeSharp.Core.Executors.Models.Shared;
+using CodeSharp.Core.Executors.Models.Testing;
+using CodeSharp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Text.Json;
+
+namespace CodeSharp.EntityFramework.Configurations;
+
+public sealed class TestLogConfiguration : IEntityTypeConfiguration<TestLog>
+{
+    public void Configure(EntityTypeBuilder<TestLog> builder)
+    {
+        var jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var codeAnalysisConverter = new ValueConverter<CodeAnalysisReport, string>(
+                   v => JsonSerializer.Serialize(v, jsonSerializerOptions),
+                   v => JsonSerializer.Deserialize<CodeAnalysisReport>(v, jsonSerializerOptions) ?? new CodeAnalysisReport());
+
+        var testResultsConverter = new ValueConverter<IList<TestingResult>, string>(
+                   v => JsonSerializer.Serialize(v, jsonSerializerOptions),
+                   v => JsonSerializer.Deserialize<IList<TestingResult>>(v, jsonSerializerOptions) ?? new List<TestingResult>());
+
+        builder.Property(e => e.CodeReport)
+            .HasConversion(codeAnalysisConverter)
+            .IsUnicode(false);
+
+        builder.Property(e => e.TestResults)
+            .HasConversion(testResultsConverter)
+            .IsUnicode(false);
+
+        builder
+            .HasKey(e => e.Id);
+    }
+}
