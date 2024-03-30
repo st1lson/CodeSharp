35dd1d161535931ad9c3af9f45c5f7ff49a0de12
Maksym <rendon4ik@gmail.com>
2024-03-30 16:31:48 +0200

    Remove useless classes

--- src/Runner/CodeSharp.Executor/CodeSharp.Executor/Infrastructure/Interfaces/ICompilationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CodeSharp.Executor.Contracts.Shared;
-
-namespace CodeSharp.Executor.Infrastructure.Interfaces;
-
-public interface ICompilationService
-{
-    Task<ProcessExecution> CompileTestsAsync(TimeSpan? maxDuration = default, long? maxRamUsage = default, CancellationToken cancellationToken = default);
-    Task<ProcessExecution> CompileExecutableAsync(TimeSpan? maxDuration = default, long? maxRamUsage = default, CancellationToken cancellationToken = default);
-}
