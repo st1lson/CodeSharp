06d8ab05f9f88c169e11b3e9efb224053c01323c
Maksym <rendon4ik@gmail.com>
2024-04-04 21:03:46 +0300

    Update some models

--- src/CodeSharp.Core/Executors/Models/Compilation/CompilationResponse.cs
+++ src/CodeSharp.Core/Executors/Models/Compilation/CompilationResponse.cs
@@ -1,12 +1,14 @@
-﻿using System.Text.Json;
-using CodeSharp.Core.Executors.Models.Shared;
+﻿using CodeSharp.Core.Executors.Models.Shared;
+using System.Text.Json;
 
 namespace CodeSharp.Core.Executors.Models.Compilation;
 
 public class CompilationResponse
 {
-    public bool Success { get; }
-    public TimeSpan Duration { get; init; }
+    public bool CompiledSuccessfully { get; init; }
+    public bool? ExecutedSuccessfully { get; init; }
+    public TimeSpan CompilationDuration { get; init; }
+    public TimeSpan? ExecutionDuration { get; init; }
     public string? Output { get; init; }
     public required CodeAnalysisReport CodeReport { get; init; }
 
